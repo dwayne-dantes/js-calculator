01dedfdca2b31908606785d60aa1ce93d0811022
dwayne-dantes <apdullahkaraarslan026@gmail.com>
2026-01-15 17:12:22 +0300

    Add .gitignore Add defineState function Add formatResult function Update button function Update handleButton Update HTML Update CSS

--- dom.js
+++ dom.js
@@ -1,42 +1,78 @@
-import { eveluate } from "./engine.js";
+import { evaluate } from "./src/parser/engine.js";
 
 const buttons = document.querySelectorAll("button");
 
-let currentDisplay = "";
-let expression ="";
-
+let state = defineState();
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
-    let value = button.dataset.value;
-    handleButton(value, display)    
+    let symbol = button.dataset.value;
+    state = handleButton(symbol, state.currentDisplay, state.oldExpression, state.expression, state.expressionDisplay, state.error);
+    document.getElementById("top-expression").textContent = state.expressionDisplay;
+    document.getElementById("bottom-expression").textContent = state.currentDisplay;
   });
 });
 
 
-function handleButton(symbol, display) {
-  if (symbol === "=") {
+function handleButton(symbol, currentDisplay, oldExpression, expression, expressionDisplay, error) {
+  if (symbol === "=" && currentDisplay !== "" && error === "") {
     try {
-      expression = currentDisplay;
-      const result = eveluate(expression);
-      display.value = result;
-      expression = result;
-      currentDisplay= result;
+      oldExpression = expression
+      expressionDisplay = currentDisplay;
+      const result = evaluate(oldExpression);
+      oldExpression = result;
+      currentDisplay = formatResult(result);
     }
     catch (err) {
-      display.value = err.message;
+      console.log(err.message);
+      error = err.message;
+      currentDisplay = "Error";
       expression = "";
+      oldExpression = "";
+      expressionDisplay = "";
     }
   }
   if (symbol === "c") {
     currentDisplay = ""
     expression = "";
-    display.value = ""
-    return;
+    oldExpression = "";
+    expressionDisplay= "";
+    error = "";
   }
-  else if (!["=", "c", "b"].includes(symbol)) {
-    currentDisplay += symbol
-    display.value = currentDisplay 
-    display.scrollLeft = display.scrollWidth;
+  else if (symbol === "b" && error === "") {
+    currentDisplay = currentDisplay.slice(0, currentDisplay.length-1);
+    if (expression !== "") {
+      expression = expression.slice(0, expression.length-1);
+    }
   }
+  else if (!["=", "c", "b"].includes(symbol) && error === "") {
+    currentDisplay += symbol;
+    expression += symbol;
+  }
+
+  return {
+    oldExpression: oldExpression,
+    expression: expression,
+    currentDisplay: currentDisplay,
+    expressionDisplay: expressionDisplay,
+    error: error
+  }
+}
+
+
+function defineState() {
+  return {
+    oldExpression: "",
+    expression: "",
+    currentDisplay: "",
+    expressionDisplay: "",
+    error: ""
+  }
+}
+
+
+function formatResult(expression, maxDecimals = 6) {
+    let str = Number(expression).toFixed(maxDecimals);   
+    str = str.replace(/\.?0+$/, '');      
+    return str;
 }
